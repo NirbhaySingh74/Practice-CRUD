e771ffd55306231b0eb39fcac3323b87cd8b0815
Nirbhay <nirbhaysingh943012@gmail.com>
2024-04-17 16:57:13 +0530

    connected-atlas

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 import express from "express";
-import { db } from "./db/db.js";
+import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import route from "./routes/route.js";
 import menurouter from "./routes/menuRouter.js";
+import mongoose from "mongoose";
 const app = express();
-const PORT = 8000;
-
+const PORT = process.env.PORT;
+dotenv.config();
 app.use(bodyParser.json());
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("server connected");
+    app.listen(PORT, () => {
+      console.log(`server is running ${PORT}`);
+    });
+  })
+  .catch((err) => console.log("server not connected", err));
 
 app.use("/details", route);
 app.use("/menu", menurouter);
